3019ad74a76447245a9ef6682605b744e448921e
plaski <maciejploskonka@gmail.com>
2017-11-05 11:26:40 +0100

    ready app

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-final-udemy
+
+Vue.js project from Udemy course 'Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)' by Maximilian Schwarzmüller
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+  export default {
+    components: {
+      appHeader: Header
+    },
+    created() {
+      this.$store.dispatch('initStocks');
+    }
+  }
+</script>
+
+<style>
+  body {
+    padding: 30px 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in .2s ease-out forwards;
+  }
+  .slide-leave-active {
+    animation: slide-out .2s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(-500px);
+      opacity: 0;
+    }
+    to {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateX(0);
+      opacity: 1;
+    }
+    to {
+      transform: translateX(-500px);
+      opacity: 0;
+    }
+  }
+</style>
